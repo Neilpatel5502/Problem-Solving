7c0261ebe9fa0170cd3421b5926dc8be3e0a4cdb
Neel Patel <nilu.neelpatel@gmail.com>
2025-04-14 16:45:44 -0400

    [Leetcode] Added 138. Copy List with Random Pointer

--- Leetcode/Linked-List/ListNode.py
+++ Leetcode/Linked-List/ListNode.py
@@ -34,3 +34,46 @@ def print_linked_list(head):
         visited.add(head)
         head = head.next
     return res
+
+
+
+# Definition for a Node with random pointer
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = x
+        self.next = next
+        self.random = random
+
+# Helper: Build linked list with random pointers
+def create_random_linked_list(arr):
+    if not arr:
+        return None
+    nodes = [Node(val) for val, _ in arr]
+    for i, (_, rand_idx) in enumerate(arr):
+        if i + 1 < len(nodes):
+            nodes[i].next = nodes[i + 1]
+        if rand_idx is not None:
+            nodes[i].random = nodes[rand_idx]
+    return nodes[0]
+
+# Helper: Print linked list with random pointers
+def print_random_linked_list(head):
+    res = []
+    node_to_index = {}
+    index = 0
+    curr = head
+
+    # First pass: assign indices to nodes
+    while curr:
+        node_to_index[curr] = index
+        curr = curr.next
+        index += 1
+
+    # Second pass: build output list with [val, random_index]
+    curr = head
+    while curr:
+        rand_idx = node_to_index[curr.random] if curr.random else None
+        res.append([curr.val, rand_idx])
+        curr = curr.next
+
+    return res
